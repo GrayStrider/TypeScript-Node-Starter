aaeabb39f30e4839b943a474e253997ad4f637de
Gray Strider <zhoga.ivan@gmail.com>
2019-10-07 03:59:16 +1100

    move to one file

--- src/changeStreams/User.ts
+++ src/changeStreams/User.ts
@@ -1,15 +1,15 @@
-import {User} from '../models/User'
-import {pubsub} from '../app'
-import {fromEvent, from} from 'rxjs'
+import { User } from '../models/User'
+import { fromEvent } from 'rxjs'
+import { pubsub } from '../server'
 
 export const UserChange = () => {
   // User.watch().on('change', (data) => console.log(data))
-    User.watch()
-      .on('change', (data) => {
-        // console.log(data)
-        pubsub.publish('HELLO', data.operationType)
-          .catch(console.log)
-      })
+  User.watch()
+    .on('change', (data) => {
+      // console.log(data)
+      pubsub.publish('HELLO', data.operationType)
+        .catch(console.log)
+    })
 
   const userObservable = User.watch()
   fromEvent(userObservable, 'change')
